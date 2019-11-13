537c4cde441e376fef958b40451abf747b7e1ef9
Alsan <info@alsan.cz>
2019-11-13 15:15:20 +0100

    Slouceni iss53 s master

--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -10,6 +10,7 @@ final class HomepagePresenter extends Nette\Application\UI\Presenter
     public function renderDefault()
     {
         $this->template->jmeno = 'Tomáš Vlček';
+        $this->template->vek = 42;
         $this->template->prezdivka = 'Vlčák';
     }
 }
