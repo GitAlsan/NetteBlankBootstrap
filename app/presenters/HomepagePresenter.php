cbd624c2c4e92f586b8cda530d30d3bd37649799
Alsan <info@alsan.cz>
2019-11-13 14:31:05 +0100

    novy branch iss53

--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -7,4 +7,8 @@ use Nette;
 
 final class HomepagePresenter extends Nette\Application\UI\Presenter
 {
+    public function renderDefault()
+    {
+        $this->template->jmeno = 'Tomáš Vlček';
+    }
 }
